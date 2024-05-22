ba0b8074c9077e4e8d874136e0be6235f04dcb54
anshul-singhrajput <143593203+anshul-singhrajput@users.noreply.github.com>
2024-05-22 12:59:13 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 963-057-2576 </p>
+         <p> <i class="fas fa-phone"></i> +91 957-586-4397 </p>
+         <p> <i class="fas fa-envelope"></i> anshulsinghrajput29@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> raipur, india - 492001 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Anshul Singh Rajput</span> </p>
+
+</section>
